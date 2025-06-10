a992fb003424b54dfc1aab76806f6bcd303f284e
CAIQUE BATISTA LINO <caiquelino6183@digitalcollege.internal>
2025-06-10 20:18:13 -0300

    Comitando Primeira parte do Array focado em listas

---
+++ array.js
@@ -0,0 +1,36 @@
+/* // Lista de Itens dos Comentários
+// 1 criar
+let nomes = ["Pedro", "João", "Maria", "José", "Ana", "Paulo", "Carlos", "Mariana", "Rafael", "Patrícia"];
+let carros = [];
+let idades = [12, '25', '24']
+const cidadesArgentina = ["Buenos Aires", "Córdoba", "Rosario", "Mendoza", "Tucumán", "Salta", "Santa Fe", "Entre Ríos", "Corrientes", "La Plata"];
+
+// Length retorna o numero com a quantidade de itens da lista
+console.log(`Tamanho: ${cidadesArgentina.length}`);
+
+// 2. Limpar
+console.log(idades);
+idades = []
+
+
+//3. adicionar elementos
+console.log(carros);
+//push adicionar elemento no final
+carros.push("Fusca", 3);
+console.log(carros);
+
+//4. remover elementos
+console.log(nomes);
+nomes.pop();
+console.log(nomes);
+nomes.shift();
+console.log(nomes);
+let nomePropri  */
+
+//Exemplo lista 5 frutas
+
+let frutas = ["maçã", "banana", "laranja", "uva", "abacaxi"];
+frutas.forEach(function(item){
+    console.log(item);
+    
+})
