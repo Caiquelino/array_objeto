5f2454b860c8d2e49520afb58193f57b760edd88
CAIQUE BATISTA LINO <caiquelino6183@digitalcollege.internal>
2025-06-10 21:29:13 -0300

    comitando exercicios

---
+++ meuobjeto.js
@@ -0,0 +1,15 @@
+let curso = {
+    nome: 'Caique',
+    curso: 'FullStack',
+    valor: '1000',
+    duracao: '12 meses',
+    tagCurso: 'fs25',
+    sala:{
+        cadeiras:'30',
+        computadores:'30',
+        projetor: 'true',
+        nomeSala: 'Grace Hopper'
+    }
+}
+
+console.log(curso);
